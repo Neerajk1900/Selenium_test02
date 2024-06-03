164db2e77ff406e1f783d06fae400b5fc49dab48
Neerajkishor <Neerajkishor131@gmail.com>
2024-06-03 12:47:11 +0530

    Parameters updated on pom and class file

--- src/test/java/New_jenkins/UItest.java
+++ src/test/java/New_jenkins/UItest.java
@@ -2,18 +2,35 @@ package New_jenkins;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class UItest {
 	
+	    @Parameters("Browser")
 	    @Test
-		public void Startbrowser() throws Exception {
-
-WebDriverManager.chromedriver().setup();
-WebDriver driver = new ChromeDriver();
+		public void Startbrowser(String browsername) throws Exception 
+	    {
+	    	WebDriver driver=null;
+			System.out.println("Parameter name is "+browsername);
+			if(browsername.contains("chrome"))
+			{
+              WebDriverManager.chromedriver().setup();
+               driver = new ChromeDriver();
+			}
+			else if(browsername.contains("Edge"))
+			{
+             WebDriverManager.edgedriver().setup();
+             driver = new EdgeDriver();
+			}
+			
+			
+			//WebDriverManager.chromedriver().setup();
+			//WebDriver driver = new ChromeDriver();
 			driver.manage().window().maximize();
 			Thread.sleep(2000);
 			driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
